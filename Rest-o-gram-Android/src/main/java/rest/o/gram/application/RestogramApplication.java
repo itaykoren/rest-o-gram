633cab7bc8be60800cee3d6ac7c6a19844d729df
Itay <itaykore@gmail.com>
2013-04-20 13:43:03 +0300

    adding RestogramApplication class instead of the one with the typo

---
+++ Rest-o-gram-Android/src/main/java/rest/o/gram/application/RestogramApplication.java
@@ -0,0 +1,20 @@
+package rest.o.gram.application;
+
+import android.app.Application;
+import rest.o.gram.client.RestogramClient;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Itay
+ * Date: 20/04/13
+ */
+public class RestogramApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize client
+        RestogramClient.getInstance().initialize(getBaseContext());
+    }
+}
