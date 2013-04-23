5a668d9de866f382864dbd72167513e03affb3fe
Itay <itaykore@gmail.com>
2013-04-24 00:59:42 +0300

    removed (hopefully) unnecessary file

--- Rest-o-gram-Android/gen/rest/o/gram/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package rest.o.gram;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
