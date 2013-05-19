e9461490d947a66c84ac30160ca317f69fd2465d
Itay <itaykore@gmail.com>
2013-05-20 00:12:46 +0300

    facebook login and session management

---
+++ Rest-o-gram-Server/Rest-o-gram-Server-webapp/src/main/java/rest/o/gram/transport/JsonRpcAuthServlet.java
@@ -0,0 +1,45 @@
+package rest.o.gram.transport;
+
+import org.json.rpc.server.JsonRpcExecutor;
+import org.json.rpc.server.JsonRpcServletTransport;
+import rest.o.gram.iservice.RestogramAuthService;
+import rest.o.gram.service.RestogramAuthServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Or
+ * Date: 5/19/13
+ */
+public class JsonRpcAuthServlet extends HttpServlet {
+    public JsonRpcAuthServlet() {
+        executor = bind();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        executor.execute(new JsonRpcServletTransport(req, resp));
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("auth-service");
+    }
+
+    private JsonRpcExecutor bind() {
+        JsonRpcExecutor executor = new JsonRpcExecutor();
+
+        RestogramAuthService impl = new RestogramAuthServiceImpl();
+        executor.addHandler("restogram-auth", impl, RestogramAuthService.class);
+
+        // add more services here
+
+        return executor;
+    }
+
+    private final JsonRpcExecutor executor;
+}
---
+++ Rest-o-gram-Server/Rest-o-gram-Server-webapp/src/main/webapp/admin.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: or_2
+  Date: 5/17/13
+  Time: 3:34 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
