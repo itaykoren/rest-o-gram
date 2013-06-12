f82bb5b7a4b1acd4093adff006b6a799b7b80d65
Itay <itaykore@gmail.com>
2013-06-12 13:32:52 +0300

    removed obsolete method (isAccurate)

--- Rest-o-gram-Android/src/main/java/rest/o/gram/location/Utils.java
+++ Rest-o-gram-Android/src/main/java/rest/o/gram/location/Utils.java
@@ -9,14 +9,6 @@ import rest.o.gram.common.Defs;
  * Date: 5/12/13
  */
 public final class Utils {
-    public static boolean isAccurate(int accuracy, String provider) {
-        if (provider  == LocationManager.GPS_PROVIDER)
-            return  true;
-        else if (provider  == LocationManager.NETWORK_PROVIDER)
-            return accuracy <= Defs.Location.DEFAULT_FINDME_RADIUS * Defs.Location.FINDME_UNCERTAINTY_FACTOR;
-
-        return false;
-    }
 
     /**
      * Returns distance (in meters) between two given points
