46f9ae15f9471b042b740242980f4428e1154a5c
Roi <roichenz7@gmail.com>
2014-05-30 18:39:36 +0300

    added unit test

--- Rest-o-gram-Server/Rest-o-gram-Server-core/src/test/java/TestService.java
+++ Rest-o-gram-Server/Rest-o-gram-Server-core/src/test/java/TestService.java
@@ -1,11 +1,14 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import rest.o.gram.iservice.RestogramService;
+import rest.o.gram.results.VenueResult;
 import rest.o.gram.results.VenuesResult;
 import rest.o.gram.service.RestogramServiceImpl;
 
 import java.util.Arrays;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
@@ -24,6 +27,7 @@ public class TestService {
     }
 
     @Test
+    @Ignore("Involves DS manipulation")
     public void testGetNearby() {
         VenuesResult result = service.getNearby(48.853015, 2.368884, 500);
 
@@ -32,5 +36,15 @@ public class TestService {
         assertTrue(result.getResult().length > 0);
 
         System.out.println(Arrays.toString(result.getResult()));
+    }
+
+    @Test
+    @Ignore("Involves DS manipulation")
+    public void testGetInfo() {
+        VenueResult result = service.getInfo("4b2fe2b0f964a520f7f124e3");
+
+        assertNotNull(result);
+        assertNotNull(result.getResult());
+        assertEquals("Café Français", result.getResult().getName());
     }
 }
